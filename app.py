7ce31305804d355194150d218f8a65a8efe29a90
Your Name <you@example.com>
2023-08-06 00:28:46 +0400

    add todos :)

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import *
 import adapter
 
 """
-
+TODO: сделать выбор конкретного пола или случайный выбор
+TODO: сделать генерацию ФИО в одном поле
+TODO: сделать генерацию отчества, а для этого их добавить
+TODO: сделать сохранение данных в формате csv, json
+TODO: сделать генерацию скрипта для sql
 """
 app = Flask(__name__)
 columns = list(adapter.columns_person.keys()) + list(adapter.columns_address.keys())
-
+о
 @app.route('/')
 def route_main():    
     return render_template('index.html', columns=columns, name=None, gen_cols=['first', 'second', 'third'])
